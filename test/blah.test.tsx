0d3e1d02e396f687e80eb184c5b7d757e2240a32
ulifesmile <ulifesmile@protonmail.com>
2023-06-16 17:55:59 +0800

    feat: wohoox plugin init

---
+++ test/blah.test.tsx
@@ -0,0 +1,14 @@
+import { log } from '../src';
+
+describe('it', () => {
+  it('log called', () => {
+    const consoleSpy = jest.spyOn(console, "log");
+
+    log();
+
+    expect(consoleSpy).toHaveBeenCalled();
+    expect(consoleSpy).toHaveBeenCalledWith("wohoox-plugin-log");
+
+    consoleSpy.mockRestore();
+  });
+});
