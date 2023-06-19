6a905439d0a97317357773366e4f2d5357464b95
ulifesmile <ulifesmile@protonmail.com>
2023-06-19 17:09:57 +0800

    fix: lint fix

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -2,12 +2,12 @@ import { log } from '../src';
 
 describe('it', () => {
   it('log called', () => {
-    const consoleSpy = jest.spyOn(console, "log");
+    const consoleSpy = jest.spyOn(console, 'log');
 
     log();
 
     expect(consoleSpy).toHaveBeenCalled();
-    expect(consoleSpy).toHaveBeenCalledWith("wohoox-plugin-log");
+    expect(consoleSpy).toHaveBeenCalledWith('wohoox-plugin-log');
 
     consoleSpy.mockRestore();
   });
